8fdafef8b97763d9de4e996c974b9dee977dcc13
BaconErie <baconerie0001@gmail.com>
2023-06-02 22:57:55 -0400

    I can now add and remove stuff from the ignore list DISPLAY

--- app/options/page.js
+++ app/options/page.js
@@ -1,18 +1,82 @@
 'use client'; // Allows us to use event handlers. Doesn't matter if it's client or server components, this is all going to compiled to client anyway
 
+import { useState } from 'react';
+
 import styles from './options.module.css';
 import { Open_Sans } from 'next/font/google';
 
 import PrimaryButton from '../../components/PrimaryButton/PrimaryButton';
 import WebsiteDisplay from '../../components/WebsiteDisplay/WebsiteDisplay';
+import { devIndicators } from '@/next.config';
 
 const openSans = Open_Sans({
   weight: '500',
   subsets: [ 'latin' ]
 })
 
+function isBlocking() {
+  return false;
+}
+
+
+function WebsiteList({ ignoreDisplay, setIgnoreDisplay }) {
+
+  function handleRemove(website) {
+    
+
+    if (isBlocking()) {
+      const reallyDelete = confirm(`Are you sure you want to remove {website} from you ignore list? You won't be able to add it back until your block has ended!`); 
+
+      if (!reallyDelete) {
+        return;
+      }
+    }
+
+    //TODO: ACTUALLY REMOVE THE WEBSITE FROM THE IGNORE LIST
+    let newIgnoreDisplay = [...ignoreDisplay];
+    const index = newIgnoreDisplay.indexOf(website);
+    if (index > -1) { 
+      newIgnoreDisplay.splice(index, 1); 
+    }
+
+    setIgnoreDisplay(newIgnoreDisplay);
+  }
+
+  const listItems = ignoreDisplay.map(website =>
+    (<WebsiteDisplay key={website} website={website} handleRemove={handleRemove} />)
+  );
+
+  return (<>
+    <div className={styles.websiteList}>
+      {listItems}
+    </div>
+  </>);
+}
 
 export default function OptionsPage() {
+  const [ignoreDisplay, setIgnoreDisplay] = useState([]);
+  
+
+  function handleIgnoreButton() {
+    if (isBlocking()) {
+      alert('You cannot add websites to ignore while blocking is enabled!');
+      return;
+    }
+
+    const ignoreInput = document.getElementById('ignoreInput');
+    const websiteToIgnore = ignoreInput.value;
+
+    //TODO: ACTUALLY SAVE THE WEBSITES THAT ARE ADDED
+    
+    setIgnoreDisplay(ignoreDisplay.concat(websiteToIgnore));
+
+    ignoreInput.value = '';
+  }
+
+  /**********
+  MAIN EXPORT
+  **********/
+
   return (<><main className={styles.main}>
     <div>
       <h1><span className={styles.yellow}>RocketBlock</span> Settings</h1>
@@ -24,9 +88,9 @@ export default function OptionsPage() {
       <br />
       
       <div className={styles.inputWrapper}>
-        <input className={styles.inputBox} style={openSans.style} type="text" placeholder="Add a website to ignore" />
+        <input id="ignoreInput" className={styles.inputBox} style={openSans.style} type="text" placeholder="Add a website to ignore" />
 
-        <PrimaryButton onClick={() => {console.log('ignore')}}>Ignore</PrimaryButton>
+        <PrimaryButton onClick={handleIgnoreButton}>Ignore</PrimaryButton>
       </div>
 
       <p>Notes:</p>
@@ -36,12 +100,17 @@ export default function OptionsPage() {
           <li>Use * as a placeholder/wildcard</li>
       </ul>
 
-      <div className={styles.websiteList}>
-        <WebsiteDisplay website={"https://www.google.com"} />
-        <WebsiteDisplay website={"https://www.wikipedia.com"} />
-        <WebsiteDisplay website={"https://www.mit.edu"} />
-        <WebsiteDisplay website={"https://www.virginia.edu"} />
-      </div>
+      <WebsiteList ignoreDisplay={ignoreDisplay} setIgnoreDisplay={setIgnoreDisplay} />
     </div>
+
+    <div>
+      <h1>Start blocking</h1>
+
+      <h2>Block for <input type="number" style={openSans.style} className={styles.number} placeholder="hrs" /> : <input type="number" style={openSans.style} className={styles.number} placeholder="min" /> : <input type="number" style={openSans.style} className={styles.number} placeholder="sec" /></h2>
+      
+      <PrimaryButton className={styles.blockButton}>Start Blocking</PrimaryButton>
+
+    </div>
+
   </main></>)
 }
--- components/WebsiteDisplay/WebsiteDisplay.js
+++ components/WebsiteDisplay/WebsiteDisplay.js
@@ -1,10 +1,13 @@
+import { useState } from 'react';
+
 import SurfaceButton from '../SurfaceButton/SurfaceButton';
 import styles from './WebsiteDisplay.module.css';
 
-export default function WebsiteDisplay({ website }) {
+export default function WebsiteDisplay({ website, handleRemove }) {
+
   return (<>
     <div className={styles.mainDisplay}>
-      <SurfaceButton className={styles.button}>x</SurfaceButton>
+      <SurfaceButton className={styles.button} onClick={() => handleRemove(website)}>x</SurfaceButton>
       <span className={styles.span}>{website}</span>
     </div>
   </>)
